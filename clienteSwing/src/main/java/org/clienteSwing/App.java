c4b0c93eecc691150da781713f3a541df85becb1
mmaldonado <mmaldonado@interbanking.com.ar>
2017-09-14 11:36:19 -0300

    Primer commit para organizar

---
+++ clienteSwing/src/main/java/org/clienteSwing/App.java
@@ -0,0 +1,13 @@
+package org.clienteSwing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ servidor/src/main/java/org/servidor/App.java
@@ -0,0 +1,13 @@
+package org.servidor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
