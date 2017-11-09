eabb608a552b71ab95db8252a4472e066f5a930b
jmejia <jmejia@prismamp.com>
2017-11-09 00:49:45 -0300

    fix indent

--- repositorio/src/main/java/org/repositorio/dtos/CrearComandaDTO.java
+++ repositorio/src/main/java/org/repositorio/dtos/CrearComandaDTO.java
@@ -2,16 +2,13 @@ package org.repositorio.dtos;
 
 import java.util.List;
 
-
 public class CrearComandaDTO {
 
 	private List<String> platos;
 	private int numeroMesa;
 	private int estadoComanda;
 	private int idLocal;
-	
-	
-	
+
 	public CrearComandaDTO(List<String> platos, int numeroMesa, int estadoComanda, int idLocal) {
 		super();
 		this.platos = platos;
@@ -19,35 +16,41 @@ public class CrearComandaDTO {
 		this.estadoComanda = estadoComanda;
 		this.idLocal = idLocal;
 	}
+
 	public CrearComandaDTO() {
 		// TODO Auto-generated constructor stub
 	}
+
 	public List<String> getPlatos() {
 		return platos;
 	}
+
 	public void setPlatos(List<String> platos) {
 		this.platos = platos;
 	}
+
 	public int getNumeroMesa() {
 		return numeroMesa;
 	}
+
 	public void setNumeroMesa(int numeroMesa) {
 		this.numeroMesa = numeroMesa;
 	}
+
 	public int getEstadoComanda() {
 		return estadoComanda;
 	}
+
 	public void setEstadoComanda(int estadoComanda) {
 		this.estadoComanda = estadoComanda;
 	}
+
 	public int getIdLocal() {
 		return idLocal;
 	}
+
 	public void setIdLocal(int idLocal) {
 		this.idLocal = idLocal;
 	}
-	
-	
-	
-	
+
 }
