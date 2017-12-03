a275aa6d9304b2885d69b22ac027778acd0d5dbd
Vene <ratablanca_max@hotmail.com>
2017-12-03 15:21:49 -0300

    cambios productos srvlet

--- clienteWeb/src/main/java/org/clienteWeb/controller/ProductoServelet.java
+++ clienteWeb/src/main/java/org/clienteWeb/controller/ProductoServelet.java
@@ -2,37 +2,50 @@ package org.clienteWeb.controller;
 
 import java.util.List;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.repositorio.bussinessDelegate.BussinessDelegate;
 import org.repositorio.dtos.ProductosAPedirDTO;
 import org.repositorio.exceptions.BusinessException;
 
 import com.google.gson.Gson;
 
-public class ProductoServelet {
+public class ProductoServelet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private static ProductoServelet instancia;
-	
-	
 
+	public void init(ServletConfig config) throws ServletException {
+	}
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) {
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) {
+	}
 
 	public static ProductoServelet getInstancia() {
-		if(instancia==null) {
-			instancia= new ProductoServelet();
+		if (instancia == null) {
+			instancia = new ProductoServelet();
 		}
 		return instancia;
 	}
 
-	private ProductoServelet(){};
-
-
+	private ProductoServelet() {
+	};
 
 	public String listarProductosParaPedir() throws BusinessException {
 
 		List<ProductosAPedirDTO> listaP = BussinessDelegate.getInstancia().listarProductosParaPedir();
 		String Json = new Gson().toJson(listaP);
 		return Json;
-		
-		}
-	
-	
-	
+
+	}
+
 }
