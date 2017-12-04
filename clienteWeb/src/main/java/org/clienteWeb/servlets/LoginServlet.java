567c166f1e54a4e207f451251dc08b27b8b9d298
Vene <ratablanca_max@hotmail.com>
2017-12-03 21:12:37 -0300

    login super hardcore, falta redireccionamiento

--- clienteWeb/src/main/java/org/clienteWeb/servlets/LoginServlet.java
+++ clienteWeb/src/main/java/org/clienteWeb/servlets/LoginServlet.java
@@ -1,6 +1,7 @@
 package org.clienteWeb.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -17,16 +18,39 @@ public class LoginServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		req.getRequestDispatcher("jsp/login principal.jsp").forward(req, resp);
+		
+		}
 
-	}
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
-	public void doPost(HttpServletRequest req, HttpServletResponse resp) {
-
-		String email = req.getParameter("email");
+		String user = req.getParameter("usuario");
 		String pwd = req.getParameter("password");
+	
+		if ("administrador".equals(user) && "admin".equals(pwd)) {
+		
+		}
+		else if ("mozo".equals(user) && "mozo".equals(pwd)) {
+			
+		}else if("barra".equals(user) && "barra".equals(pwd)) {
+			
+		}else if ("cocina".equals(user) && "cocina".equals(pwd)) {
+			
+		}else if ("cafeteria".equals(user) && "cafeteria".equals(pwd)) {
+			
+		}else {
+			response(resp, "Login invalido");
+		}
+	}
+	
 
-		System.out.println("email: " + email + " pwd: " + pwd);
-
+	private void response(HttpServletResponse resp, String msg) throws IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<t1>" + msg + "</t1>");
+		out.println("</body>");
+		out.println("</html>");
+		
 	}
 
 }
